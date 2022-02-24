493999e93c012735b95a10f7fcd7f40dd85f9e9d
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2022-02-24 14:20:36 +0100

    impr: check if env is ready before deploy

--- github-action/app/deploy_application.go
+++ github-action/app/deploy_application.go
@@ -10,13 +10,46 @@ import (
 )
 
 func DeployApplication(qoveryAPIToken string, qoveryApplicationID string, qoveryEnvironmentID string, applicationCommitID string) error {
-
 	qoveryAPIClient := pkg.NewQoveryAPIClient(
 		&http.Client{},
 		"https://api.qovery.com",
 		qoveryAPIToken,
 		0,
 	)
+
+	timeout := time.Second * 900 // 15 minutes
+
+	// Checking deployment is not QUEUED or DEPLOYING already
+	// if so, wait for it to be ready
+	state_is_ok := false
+	var status *pkg.EnvironmentStatus
+	for start := time.Now(); time.Since(start) < timeout; {
+		status, err := qoveryAPIClient.GetEnvironmentStatus(qoveryEnvironmentID)
+		if err != nil {
+			return fmt.Errorf("error while trying to get environment status: %s", err)
+		}
+
+		// Statuses ok to start a deployment
+		if status.State == pkg.EnvStatusDeploymentError ||
+			status.State == pkg.EnvStatusStopError ||
+			status.State == pkg.EnvStatusRunning ||
+			status.State == pkg.EnvStatusRunningError ||
+			status.State == pkg.EnvStatusCancelError ||
+			status.State == pkg.EnvStatusCancelled ||
+			status.State == pkg.EnvStatusUnknown {
+			state_is_ok = true
+			break
+		}
+
+		fmt.Printf("Environment cannot accept deploy yet, state: %s\n", status.State)
+
+		time.Sleep(10 * time.Second)
+	}
+
+	// Environment state is not valid even after timeout, cannot deploy the application
+	if !state_is_ok {
+		return fmt.Errorf("error: application cannot be deployed, environment status is : %s", status.State)
+	}
 
 	// Launching deployment
 	err := qoveryAPIClient.DeployApplication(pkg.Application{ID: qoveryApplicationID, CommitID: applicationCommitID})
@@ -25,14 +58,13 @@ func DeployApplication(qoveryAPIToken string, qoveryApplicationID string, qovery
 	}
 
 	// Waiting for deployment to be OK or ERRORED with a timeout
-	timeout := time.Second * 3600
 	for start := time.Now(); time.Since(start) < timeout; {
 		status, err := qoveryAPIClient.GetEnvironmentStatus(qoveryEnvironmentID)
 		if err != nil {
 			return fmt.Errorf("error while trying to get environment status: %s", err)
 		}
 
-		fmt.Printf("Environment is %s\n", status.State)
+		fmt.Printf("Deployment ongoing: status %s\n", status.State)
 
 		if status.State == pkg.EnvStatusRunning {
 			break
