8d6d588183305956687d3b21a25a27ee28e75c89
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2022-02-24 12:19:23 +0100

    fix: get commit id from env

--- github-action/main.go
+++ github-action/main.go
@@ -12,7 +12,6 @@ import (
 var (
 	application = kingpin.New("Qovery deploy", "A command-line allowing to deploy Qovery application.")
 
-	applicationCommitID  = kingpin.Arg("app-commit-id", "Commit ID for application to deploy").Required().String()
 	qoveryOrganizationID = kingpin.Arg("qovery-org-id", "Qovery organization ID").Required().String()
 	qoveryEnvironmentID  = kingpin.Arg("qovery-env-id", "Qovery environment ID").Required().String()
 	qoveryApplicationID  = kingpin.Arg("qovery-app-id", "Qovery application ID").Required().String()
@@ -22,9 +21,15 @@ var (
 func main() {
 	kingpin.Parse()
 
-	fmt.Printf("Qovery deployment starting for commit: %s ...\n", *applicationCommitID)
+	applicationCommitID := os.Getenv("GITHUB_SHA")
+	if applicationCommitID == "" {
+		fmt.Println("error: GITHUB_SHA shouldn't be empty")
+		os.Exit(1)
+	}
 
-	err := app.DeployApplication(*qoveryAPIToken, *qoveryApplicationID, *qoveryEnvironmentID, *applicationCommitID)
+	fmt.Printf("Qovery deployment starting for commit: %s ...\n", applicationCommitID)
+
+	err := app.DeployApplication(*qoveryAPIToken, *qoveryApplicationID, *qoveryEnvironmentID, applicationCommitID)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
