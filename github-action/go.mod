c113eca015d4bbec0fcbcaccfd6e62b343129b09
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2022-02-24 11:42:06 +0100

    refactor: check required program inputs

--- github-action/go.mod
+++ github-action/go.mod
@@ -1,3 +1,9 @@
 module github-action
 
 go 1.17
+
+require (
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751 // indirect
+	github.com/alecthomas/units v0.0.0-20211218093645-b94a6e3cc137 // indirect
+	gopkg.in/alecthomas/kingpin.v2 v2.2.6 // indirect
+)
--- github-action/main.go
+++ github-action/main.go
@@ -4,19 +4,27 @@ import (
 	"fmt"
 	"os"
 
+	"gopkg.in/alecthomas/kingpin.v2"
+
 	"github-action/app"
 )
 
+var (
+	application = kingpin.New("Qovery deploy", "A command-line allowing to deploy Qovery application.")
+
+	applicationCommitID  = kingpin.Arg("app-commit-id", "Commit ID for application to deploy").Required().String()
+	qoveryOrganizationID = kingpin.Arg("qovery-org-id", "Qovery organization ID").Required().String()
+	qoveryEnvironmentID  = kingpin.Arg("qovery-env-id", "Qovery environment ID").Required().String()
+	qoveryApplicationID  = kingpin.Arg("qovery-app-id", "Qovery application ID").Required().String()
+	qoveryAPIToken       = kingpin.Arg("qovery-api-token", "Qovery API token").Required().String()
+)
+
 func main() {
-	applicationCommitID := os.Getenv("GITHUB_SHA")
-	// qoveryOrganizationID := os.Args[1]
-	qoveryEnvironmentID := os.Args[2]
-	qoveryApplicationID := os.Args[3]
-	qoveryAPIToken := os.Args[4]
+	kingpin.Parse()
 
-	fmt.Printf("Qovery deployment starting for commit: %s ...\n", applicationCommitID)
+	fmt.Printf("Qovery deployment starting for commit: %s ...\n", *applicationCommitID)
 
-	err := app.DeployApplication(qoveryAPIToken, qoveryApplicationID, qoveryEnvironmentID, applicationCommitID)
+	err := app.DeployApplication(*qoveryAPIToken, *qoveryApplicationID, *qoveryEnvironmentID, *applicationCommitID)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
