d79058511829210e5f9a29033b048e8f0ce10dc1
Benjamin Chastanier <benjamin.chastanier@gmail.com>
2022-03-11 17:54:24 +0100

    fix: unknown github.sha issue

--- github-action/main.go
+++ github-action/main.go
@@ -16,11 +16,22 @@ var (
 	qoveryEnvironmentID  = kingpin.Arg("qovery-env-id", "Qovery environment ID").Required().String()
 	qoveryApplicationID  = kingpin.Arg("qovery-app-id", "Qovery application ID").Required().String()
 	qoveryAPIToken       = kingpin.Arg("qovery-api-token", "Qovery API token").Required().String()
-	applicationCommitID  = kingpin.Arg("application-commit-id", "Application commit ID").Required().String()
+	applicationCommitID  = kingpin.Arg("application-commit-id", "Application commit ID").String()
 )
 
 func main() {
 	kingpin.Parse()
+
+	envCommitID := ""
+	if applicationCommitID == nil || *applicationCommitID == "" {
+		envCommitID = os.Getenv("GITHUB_SHA")
+		applicationCommitID = &envCommitID
+	}
+
+	if applicationCommitID == nil || *applicationCommitID == "" {
+		fmt.Println("error: commit ID shouldn't be empty: `application-commit-id` to be set in args or `GITHUB_SHA` env var to be set.")
+		os.Exit(1)
+	}
 
 	fmt.Printf("Qovery deployment starting for commit: %s ...\n", *applicationCommitID)
 
